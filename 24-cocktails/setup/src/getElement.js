31c08745bc2f04fe9836cf432aeca97780342360
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-11-07 13:26:56 +0300

    Complete Project 24 (cocktails)

--- 24-cocktails/setup/src/getElement.js
+++ 24-cocktails/setup/src/getElement.js
@@ -1,6 +1,7 @@
 const getElement = (selection) => {
   const element = document.querySelector(selection);
   if (element) return element;
+  console.log(element);
   throw new Error("no element selected");
 };
 
