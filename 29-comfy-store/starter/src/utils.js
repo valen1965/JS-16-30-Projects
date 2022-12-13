466a476afb0119098ef4abe31615198c2e58c91e
Vlad Vazhnenko <vladvazhnenko@gmail.com>
2022-12-13 20:20:08 +0200

    Section 22 Comfy Store, Filters HTML

--- 29-comfy-store/starter/src/utils.js
+++ 29-comfy-store/starter/src/utils.js
@@ -15,10 +15,29 @@ const getElement = (selection) => {
   throw new Error(`Please check "${selection}" selector, no such element exist`)
 }
 
-const formatPrice = () => {}
+const formatPrice = (price) => {
+  let formatedPrice = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: "USD"
+  }).format((price / 100).toFixed(2));
+  return formatedPrice;
+};
 
-const getStorageItem = () => {}
-const setStorageItem = () => {}
+const getStorageItem = (item) => {
+  let storageItem = localStorage.getItem(item);
+  if (storageItem) {
+    storageItem = JSON.parse(localStorage.getItem(item));
+  }
+  else {
+    storageItem = [];
+  }
+  return storageItem;
+};
+
+
+const setStorageItem = (name, item) => {
+  localStorage.setItem(name, JSON.stringify(item));
+};
 
 export {
   allProductsUrl,
